84270c6db52aade32eab647d32def400d45abaad
carlosBermudez <carlosbermudezgalvan132@gmail.com>
2023-08-22 17:40:55 +0200

    Probar pytest numeros primos

---
+++ test_primes.py
@@ -0,0 +1,11 @@
+from primes import is_prime
+
+def test_is_prime():
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(4) == False
+    assert is_prime(5) == True
+    assert is_prime(6) == False
+    assert is_prime(7) == True
+    assert is_prime(8) == False
+    assert is_prime(9) == False
