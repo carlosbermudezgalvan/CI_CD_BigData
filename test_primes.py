f8706cb1a5e28ca21cfb4826c56b9c6c5e570cff
carlosBermudez <carlosbermudezgalvan132@gmail.com>
2023-08-22 17:49:51 +0200

    Update test_primes

--- test_primes.py
+++ test_primes.py
@@ -1,11 +1,11 @@
 from primes import is_prime
 
 def test_is_prime():
-    assert is_prime(2) == True
-    assert is_prime(3) == True
-    assert is_prime(4) == False
-    assert is_prime(5) == True
-    assert is_prime(6) == False
-    assert is_prime(7) == True
-    assert is_prime(8) == False
-    assert is_prime(9) == False
+    assert is_prime(2)
+    assert is_prime(3) 
+    assert not is_prime(4)
+    assert is_prime(5) 
+    assert not is_prime(6) 
+    assert is_prime(7) 
+    assert not is_prime(8)
+    assert not is_prime(9) 
